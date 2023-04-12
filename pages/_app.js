c6c9d30a17d663175827435e817b87a715ad0ebb
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-04-12 23:33:06 +0530

    added favicon and title

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import "@/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import client from "../apollo-client";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <Head>
+        <title>Pokédex</title>
+        <link
+          rel="shortcut icon"
+          href="https://assets.pokemon.com/static2/_ui/img/favicon.ico"
+        />
+      </Head>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
   );
 }
