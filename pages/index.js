6f73f6cf5052f73dd002c3d316d072f24744d3e6
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-04-13 19:24:10 +0530

    static render 60 pokes and poke details. pagi left

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,65 @@
 import { gql } from "@apollo/client";
 import client from "../apollo-client";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
-export default function Home() {
+export default function Home({ pokemons }) {
+  const router = useRouter();
   return (
-    <main>
-      <h2>Hello</h2>
+    <main className="container mx-auto my-8 px-4">
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-6 place-content-center">
+        {pokemons.map((pokemon) => (
+          <div
+            onClick={() => {
+              router.push(`/pokemon/${pokemon.id}`);
+            }}
+            key={pokemon.id}
+            className="p-6 border rounded-md hover:border-black cursor-pointer"
+          >
+            <Image
+              src={pokemon.image}
+              alt={pokemon.name}
+              width={80}
+              height={80}
+              className="object-cover"
+            />
+            <h1 className="text-3xl text-blue-500 font-semibold">
+              {pokemon.name}
+            </h1>
+            <p className="pt-4 text-xl">{pokemon.number}</p>
+            {pokemon.types.map((type, idx) => (
+              <p className="pt-4 text-xl" key={idx}>
+                {type}
+              </p>
+            ))}
+          </div>
+        ))}
+      </div>
     </main>
   );
+}
+
+const QUERY = gql`
+  query Query($first: Int!) {
+    pokemons(first: $first) {
+      id
+      image
+      name
+      number
+      types
+    }
+  }
+`;
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: QUERY,
+    variables: { first: 60 },
+  });
+
+  return {
+    props: {
+      pokemons: data.pokemons,
+    },
+  };
 }
--- pages/pokemon/[id].jsx
+++ pages/pokemon/[id].jsx
@@ -1,12 +1,100 @@
 import React from "react";
+import client from "@/apollo-client";
 import { useRouter } from "next/router";
 import Pokemon from "@/components/Pokemon";
+import Image from "next/image";
+import { gql } from "@apollo/client";
+import { BiArrowBack } from "react-icons/bi";
+import Link from "next/link";
 
-const PokemonView = () => {
+const PokemonView = ({ pokemon }) => {
   const router = useRouter();
-  const { id } = router.query;
-
-  return <Pokemon pokeId={id} />;
+  if (router.isFallback) {
+    return (
+      <div className="container flex items-center justify-center">
+        <h2 className="text-3xl text-center font-medium">Loading...</h2>
+      </div>
+    );
+  }
+  return (
+    <section className="container py-5">
+      <Link href="/">
+        <BiArrowBack size={24} />
+      </Link>
+      <div className="mt-8">
+        <Image
+          src={pokemon.image}
+          alt={pokemon.name}
+          width={100}
+          height={100}
+        />
+        <h1>{pokemon.name}</h1>
+        <h2>{pokemon.number}</h2>
+      </div>
+    </section>
+  );
 };
+
+export async function getStaticPaths() {
+  // Call an external API endpoint to get posts
+  const { data } = await client.query({
+    query: gql`
+      query Query($first: Int!) {
+        pokemons(first: $first) {
+          id
+        }
+      }
+    `,
+    variables: { first: 20 },
+  });
+
+  // Get the paths we want to pre-render based on posts
+  const paths = data.pokemons.map((pokemon) => ({
+    params: { id: pokemon.id },
+  }));
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: true };
+}
+
+export async function getStaticProps({ params }) {
+  // params contains the post `id`.
+  // If the route is like /pokemon/i2o1io1, then params.id is i2o1io1
+  const { data } = await client.query({
+    query: gql`
+      query Query($pokemonId: String) {
+        pokemon(id: $pokemonId) {
+          id
+          image
+          name
+          height {
+            maximum
+            minimum
+          }
+          weight {
+            maximum
+            minimum
+          }
+          classification
+          types
+          weaknesses
+          resistant
+          evolutions {
+            id
+            image
+            name
+            number
+            types
+          }
+        }
+      }
+    `,
+    variables: { pokemonId: params.id },
+  });
+  const pokemon = data.pokemon;
+  // Pass pokemon data to the page via props
+  return { props: { pokemon } };
+}
 
 export default PokemonView;
